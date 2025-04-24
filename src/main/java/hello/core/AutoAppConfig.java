b4b0f9578a9b4e82f6c0f865db0e8dc9c4c77098
junhyung8795 <junhyung8795@naver.com>
2025-04-24 20:48:50 +0900

    컴포넌트 스캔의 시작 위치를 클래스 혹은 패지키 이름으로 지정하는 법

--- src/main/java/hello/core/AutoAppConfig.java
+++ src/main/java/hello/core/AutoAppConfig.java
@@ -1,12 +1,16 @@
 package hello.core;
 
 
+import hello.core.member.MemberService;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.FilterType;
 
 @Configuration
 @ComponentScan(
+//        basePackages = "hello.core",
+        basePackages = "hello.core.member",//이렇게하면 member패키지 하위에서만 컴포넌트 스캔을 한다.
+        basePackageClasses = AutoAppConfig.class,
         excludeFilters = @ComponentScan.Filter(type = FilterType.ANNOTATION, classes = Configuration.class)
 )
 public class AutoAppConfig {
